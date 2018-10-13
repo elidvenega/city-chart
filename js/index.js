3764e95c91b1d80ffe6e4747ef6c0d44b6afcd20
EVUX <venegaelid@gmail.com>
2018-10-12 21:12:12 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,74 @@
+//chart options
+const options = {
+  chart: {
+    height: 450,
+    width: "100%",
+    type: 'bar',
+    background: '#f4ff4f4',
+    foreColor: '#333'
+  },
+  series: [
+    {
+      name: 'Population',
+      data: [
+        8550405,
+        3971883,
+        2720546,
+        2296224,
+        1567442,
+        1563025,
+        1469845,
+        1394928,
+        1300092,
+        1026908
+      ]
+    }
+  ],
+  xaxis: {
+    categories: [
+      "New York",
+      "Los Angeles",
+      "Chicago",
+      "Houston",
+      "Philadelphia",
+      "Phoenix",
+      "San Antonio",
+      "San Diego",
+      "Dallas",
+      "San Jose"
+    ]
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  title: {
+    text: 'Largest Cities By Population',
+    align: 'center',
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: '25px'
+    }
+  }
+};
+
+//init Chart
+const chart = new ApexCharts(document.querySelector('#chart'), options);
+
+//render chart
+chart.render();
+
+// event method example
+document.querySelector('button').addEventListener('click',() => chart.updateOptions({
+  plotOptions: {
+    bar: {
+      horizontal: true
+    }
+  }
+
+}));
